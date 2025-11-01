bf0bafc2ee13d2b104577b07cc6e2957fb26f73e
jpc19832020-tech <jpc19832020@gmail.com>
2025-10-31 19:58:22 -0500

    tt

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 // Para el servidor, usar variables sin NEXT_PUBLIC_
 // Para el cliente, usar variables con NEXT_PUBLIC_
-const supabaseUrl = https://sqoxgyluyakgacfvptux.supabase.co
+const supabaseUrl = process.env.SUPABASE_URL || process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY || process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
